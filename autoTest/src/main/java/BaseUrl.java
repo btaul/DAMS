14ebf89229c8e3e2afaf3ceae4fe9d2fcc8d652b
Junnan-wang <Wangjunnan2015@gmail.com>
2021-03-31 16:10:45 -0500

    Add the Selenium tests

---
+++ autoTest/src/main/java/BaseUrl.java
@@ -0,0 +1,7 @@
+// Configuration class for Form Automation project
+
+
+public class BaseUrl {
+    final static String BASE_URL = "http://localhost:8084";
+    final static String CHROME_DRIVER_LOCATION = "chromedriver";
+}
