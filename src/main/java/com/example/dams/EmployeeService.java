09221bf6ec02e8af26288500d5f788015b59a44c
Junnan-wang <Wangjunnan2015@gmail.com>
2021-04-06 00:52:22 -0500

    Transfer a successful project with tables to our project

---
+++ src/main/java/com/example/dams/EmployeeService.java
@@ -0,0 +1,14 @@
+package com.example.dams;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+
+
+public interface EmployeeService {
+    List<Employee> getAllEmployees();
+    void saveEmployee(Employee employee);
+    Employee getEmployeeById(long id);
+    void deleteEmployeeById(long id);
+    Page<Employee> findPaginated(int pageNo, int pageSize, String sortField, String sortDirection);
+}
