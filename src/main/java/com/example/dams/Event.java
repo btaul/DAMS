f44893ebd7793097d242f5714b1f9bc5332ade39
Junnan-wang <Wangjunnan2015@gmail.com>
2021-04-11 16:06:29 -0500

    Update the "update" function for event.

--- src/main/java/com/example/dams/Event.java
+++ src/main/java/com/example/dams/Event.java
@@ -56,13 +56,13 @@ public class Event {
         this.description = event;
     }
 
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getStatus() {
         return status;
